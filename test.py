ce8746277ffcddb1db1c04ea9a1935881dfd9f4c
Darkys <72356480+NewDarkys@users.noreply.github.com>
2023-05-20 21:20:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import subprocess
-import re
-
-output = subprocess.check_output(['termux-clipboard-get'])
-match = re.search('level=(-?\d+)', output.decode())
-if match:
-    signal_strength = int(match.group(1))
-    print(signal_strength)
-else:
-    print('Невозможно получить мощность сигнала.')
+WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+int numberOfLevels = 5;
+WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+int level = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), numberOfLevels);
