db6763f2f6c02576a9c42ddadd1ed673c78753ee
Darkys <72356480+NewDarkys@users.noreply.github.com>
2023-05-20 21:21:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-int numberOfLevels = 5;
-WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-int level = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), numberOfLevels);
+from jnius import autoclass
+
+activity = autoclass('org.kivy.android.PythonActivity').mActivity
+wifi_info = activity.getSystemService(activity.WIFI_SERVICE).getConnectionInfo()
+signal_strength = wifi_info.getRssi()
+print(signal_strength)
