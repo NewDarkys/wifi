d1da23c36d8488bdba17e903b77ca6e710cfacfa
Darkys <72356480+NewDarkys@users.noreply.github.com>
2023-07-19 22:22:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,5 @@
-import subprocess
+from playsound import playsound
+from threading import Thread
 
-def get_wifi_networks():
-    cmd = ['iwlist', 'wlan0', 'scan'] # замените 'wlan0' на интерфейс Wi-Fi вашего устройства
-    output = subprocess.check_output(cmd).decode('utf-8')
-    networks = []
-    network = {}
-
-    for line in output.split('\n'):
-        if 'Cell' in line:
-            if network:
-                networks.append(network)
-            network = {}
-        elif 'ESSID' in line:
-            network['ssid'] = line.split(':')[1].strip().strip('"')
-        elif 'Quality' in line:
-            quality = line.split('=')[1].split()[0].split('/')
-            network['signal_level'] = round((int(quality[0]) / int(quality[1])) * 100, 2)
-    
-    if network:
-        networks.append(network)
-    
-    return networks
-
-def display_wifi_networks(networks):
-    for network in networks:
-        print(f"Network: {network['ssid']}")
-        print(f"Signal Level: {network['signal_level']} dBm")
-        print()
-
-# Получение списка сетей Wi-Fi
-wifi_networks = get_wifi_networks()
-
-# Отображение списка сетей Wi-Fi
-display_wifi_networks(wifi_networks)
+def t = Thread(target=playsound, args=("sound.mp3",))
+t.start()
